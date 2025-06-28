28a402b7a73eb4d8833351da70d57a771160fbea
Briso10-dev <kouambrice10@gmail.com>
2025-06-28 15:12:25 +0200

    Previewing a function to uptate the statu's item

--- src/components/item.jsx
+++ src/components/item.jsx
@@ -4,7 +4,8 @@ function Item({item, onDeleteItem}){
         <li className="flex items-center gap-3">
             <input 
                 type="checkbox" 
-                value={item.packed} 
+                value={item.packed}
+                onChange={()=> { } } 
                 className="h-5 w-5 accent-[#e5771f]"
             />
             <span className={item.packed ? "line-through" : ""}>
