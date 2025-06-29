5821c03b195bf93187160b5de46da9a4e2679d44
Briso10-dev <kouambrice10@gmail.com>
2025-06-29 06:49:21 +0200

    Updating stats through derived state

--- src/components/stats.jsx
+++ src/components/stats.jsx
@@ -1,4 +1,8 @@
 export default function Stats(){
+
+   const numItems = items.length; //as sooon as the items are updated(from state) the component will re-render  
+ 
+
     return(
         <footer className="bg-[#76c7ad] text-center font-bold py-8 text-3xl">
            <em>You have X items on your list, and you already packed X (X%)</em>
