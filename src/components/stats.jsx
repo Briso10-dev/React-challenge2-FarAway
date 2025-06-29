5c5f6c99417896966796d4c51cdb677ff7c5b049
Briso10-dev <kouambrice10@gmail.com>
2025-06-29 07:01:02 +0200

    Stat showing the packed percentage success

--- src/components/stats.jsx
+++ src/components/stats.jsx
@@ -2,10 +2,11 @@ export default function Stats({items}){
 
    const numItems = items.length; //as sooon as the items are updated(from state) the component will re-render  
    const numPacked = items.filter((item) => item.packed).length
+   const percentage =  Math.round((numPacked/numItems) * 100);
 
     return(
         <footer className="bg-[#76c7ad] text-center font-bold py-8 text-3xl">
-           <em>You have {numItems} items on your list, and you already packed {numPacked} (X%)</em>
+           <em>You have {numItems} items on your list, and you already packed {numPacked} ({percentage}%)</em>
         </footer>
     )
 }
