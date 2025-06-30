ddf5087aa090fe30187e6aabebc75d1234eb6e26
Briso10-dev <kouambrice10@gmail.com>
2025-06-30 17:42:50 +0200

    Confirming all items deletion success

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -27,7 +27,9 @@ function App() {
     }
 
      function deleteAllItems(){
-          setItems([]);            
+          const confirmed = window.confirm("Are you sure you want to delete all items?");
+
+          if(confirmed) setItems([]);            
     }
 
   return (
