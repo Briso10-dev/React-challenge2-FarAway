1182650924bcbe830837198aaf0b5dcbc2bc409b
Briso10-dev <kouambrice10@gmail.com>
2025-06-30 17:02:43 +0200

    Sort by items packed success

--- src/components/packing-list.jsx
+++ src/components/packing-list.jsx
@@ -10,6 +10,8 @@ export default function PackingList({items,onDeleteItem,onToggleItem}){
 
     if(sortBy == 'description') sortedItems = items.slice().sort((a,b) => a.description.localeCompare(b.description))
 
+    if(sortBy == 'packed') sortedItems = items.slice().sort((a,b) => Number(a.packed) - Number(b.packed) );    
+
     return(
         <div className="bg-[#5a3e2b] text-[#ffebb3] py-20 flex flex-col justify-between items-center gap-8 text-2xl h-[59vh]">
             <ul className="list-none w-4/5 overflow-auto grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-3 justify-center content-start">
